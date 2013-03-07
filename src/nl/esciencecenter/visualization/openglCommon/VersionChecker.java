3c51fe944090fa464b6bdbb40d801ec74cb91d41
Maarten van Meersbergen <m.vanmeersbergen@esciencecenter.nl>
2013-03-07 15:50:53 +0000

    Versionchecker added

---
+++ src/nl/esciencecenter/visualization/openglCommon/VersionChecker.java
@@ -0,0 +1,9 @@
+package nl.esciencecenter.visualization.openglCommon;
+
+import nl.esciencecenter.visualization.openglCommon.util.GLProfileSelector;
+
+public class VersionChecker {
+    public static void main(String[] args) {
+        GLProfileSelector.printAvailable();
+    }
+}
